1f045ef87265e2444cb52215ed3596d185e7556e
Anton <anton.vorobev1999@mail.ru>
2023-03-13 23:03:11 +0300

    Выполнил 5 задачу

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,14 @@ public class Main {
         Predicate<Integer> intPredicate = new Predicate<Integer>() {
             @Override
             public boolean test(Integer integer) {
-                if (integer < 0) {
-                    return false;
-                } else {
-                    return true;
-                }
+                return integer >= 0;
             }
         };
 
         System.out.println(intPredicate.test(-2));
         System.out.println(intPredicate.test(3));
 
-        Predicate<Integer> integerPredicate1 = i -> (i < 0 ? false : true);
+        Predicate<Integer> integerPredicate1 = i -> i>=0;
 
         System.out.println(integerPredicate1.test(-10));
         System.out.println(integerPredicate1.test(4));
@@ -48,13 +44,13 @@ public class Main {
         Function<Double, Long> rounding = new Function<Double, Long>() {
             @Override
             public Long apply(Double aDouble) {
-                return aDouble.longValue();
+                return Math.round(aDouble);
             }
         };
 
         System.out.println(rounding.apply(34.2));
 
-        Function<Double, Long> rounding1 = d -> d.longValue();
+        Function<Double, Long> rounding1 = d -> Math.round(d);
 
         System.out.println(rounding1.apply(22.4));
 
@@ -75,12 +71,22 @@ public class Main {
 
         // Задание 5
 
-        public static <T, U> Function<T, U> ternaryOperator(
-                Predicate<? super T> condition,
-                Function<? super T, ? extends U> ifTrue,
-                Function<? super T, ? extends U> ifFalse) {
-            // При вставке кода или перепечатывании его самостоятельно все окрашивается в красный цвет,
-            // что бы я ни делал, не понимаю в чем может быть проблема
-        }
+        Predicate<Integer> condition = i -> i%2 == 0;
+
+        Function<Integer, String> ifTrue = i -> "число " + i.toString() + " кратно двум";
+
+        Function<Integer, String> ifFalse = i -> "число " + i.toString() + " не кратно двум";
+
+        System.out.println(ternaryOperator(condition, ifTrue, ifFalse).apply(4));
+
+    }
+
+
+    public static <T, U> Function<T, U> ternaryOperator(
+            Predicate<? super T> condition,
+            Function<? super T, ? extends U> ifTrue,
+            Function<? super T, ? extends U> ifFalse) {
+        Function<T, U> trueOrFalse = p -> (condition.test(p) ? ifTrue.apply(p) : ifFalse.apply(p));
+        return trueOrFalse;
     }
 }
